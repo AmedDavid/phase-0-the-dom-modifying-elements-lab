156f30764877b3c1364b1b7e478ba82c6895ebd0
David Amedi <amukoahdavid@gmail.com>
2025-02-27 01:04:40 +0300

    Implement dynamic list creation and update header with victory message

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const element = document.createElement('div');
+document.body.appendChild(element);
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+const element2 = document.querySelector('ul');
+element2.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "David Amedi is the champion";
+document.body.appendChild(newHeader);
+
